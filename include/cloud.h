f51a6998c1c7129361dccac6586f20a797809521
sherandrei <welldryus@gmail.com>
2020-10-14 17:00:21 +0300

    point

--- include/cloud.h
+++ include/cloud.h
@@ -1,18 +1,15 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include <tuple>
-#include <cstdlib>
-
 #include "point.h"
+
 class Cloud
 {
 public:
-    Cloud()
-        : RGB({rand() % 256, rand() % 256, rand() % 256}) {}
+    Cloud();
+
 public:
-    std::tuple<int, int, int> RGB;
     std::vector<Point> points;
 };
 
-std::ostream& operator<<(std::ostream& os,const Cloud& cl);
+std::ostream& operator<<(std::ostream& os, const Cloud& cl);
